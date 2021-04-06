5bd702d0495502d5da939be44fa97ee60b8fe12f
Shashwata Saha <sahas277@gmail.com>
2021-04-06 23:32:43 +0530

    registering in admin

--- AudioFileServer/API/admin.py
+++ AudioFileServer/API/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from API.models import Song,Podcast,Audiobook
 
-# Register your models here.
+class RegAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Song, RegAdmin)
+admin.site.register(Podcast, RegAdmin)
+admin.site.register(Audiobook, RegAdmin)
--- AudioFileServer/API/views.py
+++ AudioFileServer/API/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 class Home():
     def Home_view(*args,**kwargs):
-        return HttpResponse("<h1>No Home Page</h1><p> Visit LocalHost:Port/api/")
+        return HttpResponse("<h1>No Home Page</h1><br><p> Visit LocalHost:Port/api/</p><br><p> Or if Admin visit LocalHost:Port/admin/</p>")
