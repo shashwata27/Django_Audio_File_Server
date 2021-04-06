0f4260601637367ff5dd00f4571cf4a69a5cf01a
Shashwata Saha <sahas277@gmail.com>
2021-04-07 00:47:46 +0530

    adding validators

---
+++ AudioFileServer/API/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+import datetime
+
+def validate_date(date):
+    pass
+    # if datetime.timedelta(datetime.datetime(date),datetime.datetime.now)>=10:
+    #     raise ValidationError("Date cannot be in the past")
+
+# 2021-04-06T19:34:00Z
